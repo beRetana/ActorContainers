622fa9f70bab784dd752909227de85ecf2e70e82
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2026-02-25 00:57:29 -0800

    Added Functions to the Library
    Added functions that make it easy to transfer contents to vanilla containers. Functions to clean up nulls in blueprints and template functions for C++ functionality.

--- Source/ActorContainers/Private/ActorContainersFunctionLibrary.cpp
+++ Source/ActorContainers/Private/ActorContainersFunctionLibrary.cpp
@@ -5,6 +5,23 @@ bool UActorContainersFunctionLibrary::IsValidType(const AActor* InActor, const T
 	return InActor->IsA(InClass);
 }
 
+void UActorContainersFunctionLibrary::GetAllActorsFromSet(FActorSet& InActorSet, TSet<AActor*>& OutActors)
+{
+	OutActors.Reserve(InActorSet.Num() + OutActors.Num());
+
+	for (TObjectPtr<AActor>& Actor : InActorSet)
+	{
+		OutActors.Add(Actor);
+	}
+
+	InActorSet.Empty();
+}
+
+void UActorContainersFunctionLibrary::CleanNullsFromSet(FActorSet& InActorSet)
+{
+	InActorSet.Remove(nullptr);
+}
+
 void UActorContainersFunctionLibrary::Push(FActorSet& InActorSet, AActor* InActor)
 {
 	if (IsValidType(InActor, InActorSet.ActorClass))
@@ -39,6 +56,27 @@ bool UActorContainersFunctionLibrary::ContainsSet(FActorSet& InActorSet, const A
 FActorSet UActorContainersFunctionLibrary::MakeActorSet(const TSubclassOf<AActor> ClassFilter)
 {
 	return FActorSet{ClassFilter};
+}
+
+void UActorContainersFunctionLibrary::GetAllActorsFromArray(FActorArray& InActorArray, TArray<AActor*>& OutActors)
+{
+	OutActors.Reserve(InActorArray.Num() + OutActors.Num());
+
+	for (auto& Element : InActorArray)
+	{
+		OutActors.Add(Element);
+	}
+
+	InActorArray.Empty();
+}
+
+void UActorContainersFunctionLibrary::CleanNullsFromArray(FActorArray& InActorArray)
+{
+	for (TObjectPtr<AActor>& Actor : InActorArray)
+	{
+		if (Actor != nullptr) continue;
+		InActorArray.Remove(Actor);
+	}
 }
 
 void UActorContainersFunctionLibrary::Add(FActorArray& InActorArray, AActor* InActor)
--- Source/ActorContainers/Public/ActorContainersFunctionLibrary.h
+++ Source/ActorContainers/Public/ActorContainersFunctionLibrary.h
@@ -18,14 +18,52 @@ public:
 	 * @param InClass Filter used to check if the actor is valid.
 	 * @return True if the Actor is a type of the filter given.
 	 */
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers")
 	static bool IsValidType(const AActor* InActor, const TSubclassOf<AActor>& InClass);
+
+	/**
+	 * Transfers all the actors from the ActorSet into another set.
+	 * @param InActorSet ActorSet with the information to transfer
+	 * @param OutActors Set to be filled with the information
+	 */
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorSet")
+	static void GetAllActorsFromSet(UPARAM(ref) FActorSet& InActorSet, UPARAM(ref) TSet<AActor*>& OutActors);
+
+	/**
+	 * Remove all the elements that are null in the set.
+	 * @param InActorSet The ActorSet to be cleaned.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorSet")
+	static void CleanNullsFromSet(UPARAM(ref) FActorSet& InActorSet);
+
+	/**
+	 * Transfers Actors from the InActorSet into the OutActorSet.
+	 * @tparam T A type derived from AActor
+	 * @param InActorSet The Set with the information
+	 * @param OutActorSet The Set information will be transferred to.
+	 */
+	template<typename T>
+	static void GetAllActorsFromSet(FActorSet& InActorSet, TSet<T*>& OutActorSet)
+	{
+		static_assert(TIsDerivedFrom<T, AActor>::Value, "Actor should be derived from AActor");
+
+		for (auto Actor : InActorSet)
+		{
+			if (Actor->IsA(T::StaticClass()))
+			{
+				OutActorSet.Add(Actor);
+			}
+		}
+
+		InActorSet.Empty();
+	}
 
 	/**
 	 * Adds the actor into the set by reference if it passes the filter.
 	 * @param InActorSet Set passed by reference that might have the actor added to it.
 	 * @param InActor Actor that might be added to the set.
 	 */
-	UFUNCTION(BlueprintCallable, Category = "_Workflow Utilities|ActorContainers|ActorSet")
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorSet")
 	static void Push(UPARAM(ref) FActorSet& InActorSet, AActor* InActor);
 
 	/**
@@ -34,14 +72,14 @@ public:
 	 * @param InActor The Actor being removed from the set.
 	 * @return The index at which the element was found.
 	 */
-	UFUNCTION(BlueprintCallable, Category = "_Workflow Utilities|ActorContainers|ActorSet")
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorSet")
 	static int PopElement(UPARAM(ref) FActorSet& InActorSet, AActor* InActor);
 
 	/**
 	 * Empties the ActorSet given.
 	 * @param InActorSet 
 	 */
-	UFUNCTION(BlueprintCallable, Category = "Workflow Utilities|ActorContainers|ActorSet")
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorSet")
 	static void EmptySet(UPARAM(ref) FActorSet& InActorSet);
 
 	/**
@@ -50,7 +88,7 @@ public:
 	 * @param InActor The Actor being checked for in the ActorSet.
 	 * @return True if the Actor given is in the ActorSet False otherwise.
 	 */
-	UFUNCTION(BlueprintCallable, Category = "Workflow Utilities|ActorContainers|ActorSet")
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorSet")
 	static bool ContainsSet(UPARAM(ref) FActorSet& InActorSet, const AActor* InActor);
 
 	/**
@@ -58,15 +96,51 @@ public:
 	 * @param ClassFilter The default filter the ActorSet should have.
 	 * @return A new empty ActorSet.
 	 */
-	UFUNCTION(BlueprintCallable, Category = "Workflow Utilities|ActorContainers|ActorSet")
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorSet")
 	static FActorSet MakeActorSet(const TSubclassOf<AActor> ClassFilter = nullptr);
 
+	/**
+	 * Transfers the Actors in InActorArray to OutActors
+	 * @param InActorArray The array with the Actors to transfer
+	 * @param OutActors The array to transfers the Actors to
+	 */
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorArray")
+	static void GetAllActorsFromArray(UPARAM(ref) FActorArray& InActorArray, UPARAM(ref) TArray<AActor*>& OutActors);
+
+	/**
+	 * Transfers the Actors in InActorArray to OutActors
+	 * @tparam T Must derive from AActor
+	 * @param InActorArray The array with the Actors to transfer
+	 * @param OutActors The array to transfers the Actors to
+	 */
+	template<typename T>
+	static void GetAllActorsFromArray(FActorArray& InActorArray, TArray<T>& OutActors)
+	{
+		static_assert(TIsDerivedFrom<T, AActor>::Value, "Actor should be derived");
+		OutActors.Reserve(InActorArray.Num() + OutActors.Num());
+		for (auto& Actor : InActorArray)
+		{
+			if (Actor->IsA(T::StaticClass()))
+			{
+				OutActors.Add(Actor);
+			}
+		}
+		InActorArray.Empty();
+	}
+
+	/**
+	 * Removes all the Null pointers in the array.
+	 * @param InActorArray Array to be cleaned
+	 */
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorArray")
+	static void CleanNullsFromArray(UPARAM(ref) FActorArray& InActorArray);
+	
 	/**
 	 * Adds the given Actor if it passes the filter in the ActorArray.
 	 * @param InActorArray The ActorArray the actor is being added to.
 	 * @param InActor The actor added to the ActorArray.
 	 */
-	UFUNCTION(BlueprintCallable, Category = "Workflow Utilities|SmartTypes|SmartArray")
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorArray")
 	static void Add(UPARAM(ref) FActorArray& InActorArray, AActor* InActor);
 
 	/**
@@ -74,7 +148,7 @@ public:
 	 * @param InActorArray The ActorArray the actor is being removed from.
 	 * @param InActor The actor to be removed from the ActorArray.
 	 */
-	UFUNCTION(BlueprintCallable, Category = "Workflow Utilities|SmartTypes|SmartArray")
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorArray")
 	static void Remove(UPARAM(ref) FActorArray& InActorArray, AActor* InActor);
 
 	/**
@@ -82,14 +156,14 @@ public:
 	 * @param InActorArray The ActorArray the actor is being removed from.
 	 * @param InIndex The index to look at when removing the actor.
 	 */
-	UFUNCTION(BlueprintCallable, Category = "Workflow Utilities|SmartTypes|SmartArray")
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorArray")
 	static void RemoveAt(UPARAM(ref) FActorArray& InActorArray, int InIndex);
 
 	/**
 	 * Empties the ActorArray given.
 	 * @param InActorArray The ActorArray to be emptied.
 	 */
-	UFUNCTION(BlueprintCallable, Category = "Workflow Utilities|SmartTypes|SmartArray")
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorArray")
 	static void EmptyArray(UPARAM(ref) FActorArray& InActorArray);
 
 	/**
@@ -98,7 +172,7 @@ public:
 	 * @param InActor The actor to be looked for in the ActorArray.
 	 * @return True if the actor is in the ActorArray, False otherwise.
 	 */
-	UFUNCTION(BlueprintCallable, Category = "Workflow Utilities|SmartTypes|SmartArray")
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorArray")
 	static bool ContainsArray(UPARAM(ref) FActorArray& InActorArray, AActor* InActor);
 
 	/**
@@ -106,6 +180,6 @@ public:
 	 * @param ClassFilter The filter the ActorArray should have.
 	 * @return An empty ActorArray
 	 */
-	UFUNCTION(BlueprintCallable, Category = "Workflow Utilities|SmartTypes|SmartArray")
+	UFUNCTION(BlueprintCallable, Category = "ActorContainers|ActorArray")
 	static FActorArray MakeActorArray(const TSubclassOf<AActor> ClassFilter = nullptr);
 };
